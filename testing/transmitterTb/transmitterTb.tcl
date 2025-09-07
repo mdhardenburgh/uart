c3d7616d1deca82217b02df7faee541493785f4b
matthew <matthew@hardenburgh.io>
2025-09-06 23:55:11 -0700

    include dirs are determined by cmake file and not hardcoded into vivado tcl file

--- testing/transmitterTb/transmitterTb.tcl
+++ testing/transmitterTb/transmitterTb.tcl
@@ -10,31 +10,42 @@ puts "PWD is [pwd]"
 set projRoot [pwd]
 
 # Build the include args for xvlog
-set inc_dirs   [list \
-    [file join $projRoot src] \
-    [file join $projRoot testing/transmitterTb] \
-]
-set inc_args {}
-foreach d $inc_dirs { lappend inc_args -i $d }
+#set inc_dirs   [list \
+#    [file join $projRoot src] \
+#    [file join $projRoot testing/transmitterTb] \
+#]
+#set inc_args {}
+#foreach d $inc_dirs { lappend inc_args -i $d }
 
 # Absolute path to this script
 set script_path [info script]
 # Directory containing this script
 set script_dir [file dirname $script_path]
 puts "Script directory: $script_dir"
-set sourceList [file join $script_dir "sourceList.txt"]
 
+set sourceList [file join $script_dir "sourceList.txt"]
+set includeDirs [file join $script_dir "includeDirs.txt"]
+
+# slurp up source list
 set fh [open $sourceList r]
-set contents [read $fh]
+set sourceListContents [read $fh]
+close $fh
+
+set fh [open $includeDirs r]
+set includeDirListContents [read $fh]
 close $fh
 
 # If your file is newline-separated (recommended):
-set srcFiles [split $contents "\n"]
+set srcFiles [split $sourceListContents "\n"]
+
+set includeList [split $sourceListContents "\n"]
+set inc_args {}
+foreach d $includeDirListContents { lappend inc_args -i $d }
 
 # Drop empties
 set clean {}
 foreach f $srcFiles {if {$f ne ""} { lappend clean $f }}
-puts "xvlog includes: $inc_dirs"
+puts "xvlog include args: $inc_args"
 
 # last thing, cd to build directory
 cd $script_dir
